833b037951892af358659c8eb4104345b04b1417
xiehangyun <631978355@qq.com>
2019-11-19 16:19:22 +0800

    用户登录功能实现

---
+++ web_back/js/tutail/user.js
@@ -0,0 +1,14 @@
+var url = "http://192.168.43.33:8000/";
+
+
+
+
+var user = {
+    login:function (userName,passWord,fn) {
+        $.post(url+"admin/login",{user_name:userName,password:passWord},
+        function (res) {
+            fn(res)
+        })
+    }
+    get
+}
