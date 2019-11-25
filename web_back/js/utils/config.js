7a790634e632dfc61967e05935660309d8aafec1
xiehangyun <631978355@qq.com>
2019-11-25 20:26:21 +0800

    后端页面ES6代码重写

--- web_back/js/utils/config.js
+++ web_back/js/utils/config.js
@@ -1,18 +1,18 @@
-var url = "http://192.168.43.33:8000/";
+const URL = "http://192.168.43.33:8000/";
 
-var PORTCONFIG = {
-    logIn: url + "admin/login",
-    getUser: url + 'admin/getuser',
-    logOut: url + "admin/logout",
+const PORTCONFIG = {
+    logIn: URL + "admin/login",
+    getUser: URL + 'admin/getuser',
+    logOut: URL + "admin/logout",
 
 
-    search: url + "admin/category_search",
-    delete: url + "admin/category_delete",
-    edit: url + "admin/category_edit",
-    add: url + "admin/category_add",
+    search: URL + "admin/category_search",
+    delete: URL + "admin/category_delete",
+    edit: URL + "admin/category_edit",
+    add: URL + "admin/category_add",
 
-    article_search: url + "admin/search",
-    article_delete: url + "admin/article_delete",
-    article_publish: url + "admin/article_publish",
-    article_edit: url + "admin/article_edit"
+    article_search: URL + "admin/search",
+    article_delete: URL + "admin/article_delete",
+    article_publish: URL + "admin/article_publish",
+    article_edit: URL + "admin/article_edit"
 }
--- web_back/js/utils/user.js
+++ web_back/js/utils/user.js
@@ -1,19 +1,8 @@
 
-var user = {
-    login:function (userName,passWord,fn) {
-        $.post(PORTCONFIG.logIn,{user_name:userName,password:passWord},
-        function (res) {
-            fn(res);
-        });
-    },
-    getUser:function (fn) {
-        $.get(PORTCONFIG.getUser,function (res) {
-            fn(res);
-        });
-    },
-    logOut:function (fn) {
-        $.post(PORTCONFIG.logOut,function () {
-            fn();
-        });
-    }
+const USER = {
+    login: (userName, passWord, fn) => { $.post(PORTCONFIG.logIn, { user_name: userName, password: passWord }, (res) => { fn(res); }); },
+    getUser: (fn) => { $.get(PORTCONFIG.getUser, function (res) { fn(res); }); },
+    logOut: (fn) => { $.post(PORTCONFIG.logOut, () => { fn(); }); }
 }
+
+let {login,getUser,logOut} = USER;
